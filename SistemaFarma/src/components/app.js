b15b234228b19ede36403c8f0b6fba1fdfa9d925
DayanaRuidiaz <dayana_camargo23171@elpoli.edu.co>
2019-06-21 01:41:23 -0500

    Pantalla registrar persona

--- SistemaFarma/src/components/app.js
+++ SistemaFarma/src/components/app.js
@@ -4,7 +4,8 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Layout from './Layout'
 import Home from '../pages/Home';
 import ProfileNew from '../pages/Proveedores';
-import login from '../pages/Login'
+import login from '../pages/Login';
+import Personas from '../pages/Personas';
 
 
 
@@ -16,6 +17,7 @@ function AppSisfarma(){
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={login} />
                     <Route exact path="/Menu/proveedores" component={ProfileNew} />
+                    <Route exact path="/Menu/personas" component={Personas} />
                 </Switch>
             </Layout>
         </BrowserRouter>
